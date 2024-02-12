55272c64cdca80af6c7ae62330771868b66b1cb6
opatricpires <patric.pires@outlook.com>
2024-02-12 17:37:55 -0300

    start project

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-white text-xl font-heading">React Native!</Text>
+    </View>
+  );
+}
