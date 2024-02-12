c06a666391577aa57101316c884fd200d0d1d98f
opatricpires <patric.pires@outlook.com>
2024-02-12 18:33:08 -0300

    feat: add category component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,36 @@
+import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { View } from "react-native";
+import { View, FlatList } from "react-native";
+
+import { CATEGORIES } from "@/utils/data/products";
+import { useState } from "react";
 
 export default function App() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  function handleCategorySelect(selectedCategory: string) {
+    return setCategory(selectedCategory);
+  }
+
   return (
     <View>
-      <Header title="Cardápio" cartQuatityItems={10} />
+      <Header title="Faça seu pedido" />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        className="max-h-10- mt-5"
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   );
 }
