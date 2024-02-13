a3382a3a991a377eadb150fa678cb6365a1bb980
opatricpires <patric.pires@outlook.com>
2024-02-13 12:30:29 -0300

    feat: persist data on phone

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,5 +1,8 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import * as cartInMemory from "./helpers/cart-in-memory";
 
 export type ProductCartProps = ProductProps & {
@@ -12,14 +15,22 @@ type StateProps = {
   remove: (prodductId: string) => void;
 };
 
-export const useCartStore = create<StateProps>((set) => ({
-  products: [],
-  add: (product: ProductProps) =>
-    set((state) => ({
-      products: cartInMemory.add(state.products, product),
-    })),
-  remove: (productId: string) =>
-    set((state) => ({
-      products: cartInMemory.remove(state.products, productId),
-    })),
-}));
+export const useCartStore = create(
+  persist<StateProps>(
+    (set) => ({
+      products: [],
+      add: (product: ProductProps) =>
+        set((state) => ({
+          products: cartInMemory.add(state.products, product),
+        })),
+      remove: (productId: string) =>
+        set((state) => ({
+          products: cartInMemory.remove(state.products, productId),
+        })),
+    }),
+    {
+      name: "nlw-expert:cart",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
