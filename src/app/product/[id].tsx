1d65c09aafd25b2c590f0f662717b3e8a5f8c1f9
opatricpires <patric.pires@outlook.com>
2024-02-13 02:29:16 -0300

    refactor: product page

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,5 +1,5 @@
 import { Image, View, Text } from "react-native";
-import { useLocalSearchParams, useNavigation } from "expo-router";
+import { useLocalSearchParams, useNavigation, Redirect } from "expo-router";
 import { PRODUCTS } from "@/utils/data/products";
 import { formatCurrency } from "@/utils/functions/format-currency";
 import { Button } from "@/components/button";
@@ -12,11 +12,17 @@ export default function Product() {
   const navigation = useNavigation();
   const { id } = useLocalSearchParams();
 
-  const product = PRODUCTS.filter((item) => item.id === id)[0];
+  const product = PRODUCTS.find((item) => item.id === id);
 
   function handleAddToCart() {
-    cartStore.add(product);
-    navigation.goBack();
+    if (product) {
+      cartStore.add(product);
+      navigation.goBack();
+    }
+  }
+
+  if (!product) {
+    return <Redirect href="/" />;
   }
 
   return (
@@ -27,7 +33,8 @@ export default function Product() {
         resizeMode="cover"
       />
       <View className="p-5 mt-8 flex-1">
-        <Text className="text-lime-400 text-2xl font-heading">
+        <Text className="text-white text-xl font-heading">{product.title}</Text>
+        <Text className="text-lime-400 text-2xl font-heading my-2">
           {formatCurrency(product.price)}
         </Text>
         <Text className="text-slate-400 font-body text-base leading-6 mb-6">
